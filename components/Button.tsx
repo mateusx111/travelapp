84b68b9c6e3125c13d89da91297ce5ec4ce91b32
Mateus Souza <msouza954@gmail.com>
2023-10-23 09:54:37 -0300

    feat: ,GetApp

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface ButtonProps {
-  type: 'button'|'submit'
-  title: string
-  icon?: string
-  variant: string
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
 }
-export const Button = ({ type, title, icon, variant}: ButtonProps) => {
+export const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
-    <button 
-    className={`flexCenter gap-3 rounded-full border ${variant}`}
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-    <label className="bold-16 whitespace-nowrap">{title}</label>  
+      <label className="bold-16 whitespace-nowrap cursor-auto">{title}</label>
     </button>
-  )
-}
+  );
+};
